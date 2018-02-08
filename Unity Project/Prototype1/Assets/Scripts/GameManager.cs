40b53a406a0605fba0458e6b263af616453755d9
Daniel Pokladek <danielpokladek@gmail.com>
2018-02-08 03:58:41 +0000

    Started working on the GameManager.
    I have started working on the GameManager, with it finished we will be able to create multiple prototypes of the game, and have them tested out/playtested by other people.

---
+++ Unity Project/Prototype1/Assets/Scripts/GameManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * GameManger Script
+ * 
+ * This is the GameManager script, it is used to control the current state of the game.
+ * Ideally this also controls the players, but does not local variables for the players,
+ * so it will provide a link between the players but things like health will be stored locally.
+ * 
+ * Once the game idea expands, this script will be used to handle more and more things,
+ * most likely it will have its own functions and such; ideally have it work with a player manager,
+ * so this way we can keep this script neater. For now, most things will be done here for the prototypes.
+ * 
+*/
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager Instance;
+
+    // Most of the variables set below are just placeholders,
+    // they are there to lay foundation of the script to later build upon.
+    // Some of them will be removed, and more might be added.
+
+    [Header("Scene Settings")]
+    public Camera mapCamera;
+    public GameObject playerOnePrefab;
+    public GameObject playerTwoPrefab;
+
+    [Header("Runtime Settings")]
+    [Tooltip("This is the force that will be applied to the projectile, depending in which direction it will be facing.")]
+    [SerializeField] private float windStrenght;
+
+
+    private void Awake()
+    {
+        if (Instance == this)
+            Instance = this;
+        else if (Instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+		
+	}
+}
