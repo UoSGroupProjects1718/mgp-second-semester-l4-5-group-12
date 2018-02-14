bb2e40696a2645292b9dd728f05e30a36d0ae687
Daniel Smith <dsmith3855195@gmail.com>
2018-02-14 14:22:53 +0000

    Made a start on the physics
    Having issues getting 2D impulses to work. Not sure if this commit or the previous one contained the tag checking task. The previous commit was from the other Daniel's account because Github Desktop is strange.
    Early sprites might be useful as I would be able to work out what shape the blocks need to be.
    Daniel Smith

--- Unity Project/Prototype1/Assets/Scripts/Player Scripts/PlayerHealth.cs
+++ Unity Project/Prototype1/Assets/Scripts/Player Scripts/PlayerHealth.cs
@@ -8,6 +8,7 @@ public class PlayerHealth : MonoBehaviour {
 
     [SerializeField] private int maxHealth = 100;
     [SerializeField] private int ProjectileDamage = 10;
+    [SerializeField] private string tagName;
 
     private void Start()
     {
@@ -16,12 +17,15 @@ public class PlayerHealth : MonoBehaviour {
 
     void OnCollisionEnter2D(Collision2D collider)
     {
-        currentHealth -= ProjectileDamage;
-        Destroy(collider.gameObject);
-        // This is where the attacking player's turn would end, 
-        // assuming that we only shoot one projectile per turn.
-        // Power-ups could increase the number of projectiles,
-        // so maybe change the location of this green text wall.
+        if (collider.gameObject.tag == tagName)
+        {
+            currentHealth -= ProjectileDamage;
+            Destroy(collider.gameObject);
+            // This is where the attacking player's turn would end, 
+            // assuming that we only shoot one projectile per turn.
+            // Power-ups could increase the number of projectiles,
+            // so maybe change the location of this green text wall.
+        }
     }
 
 }
