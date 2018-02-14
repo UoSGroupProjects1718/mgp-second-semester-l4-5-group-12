6b75f035387c0210ac3ef7466b1ea7bbd2b7cfb0
Daniel Pokladek <danielpokladek@gmail.com>
2018-02-14 11:18:48 +0000

    Created player health script
    Only a basic script at the moment. When an object collides with the base, health is deducted.

---
+++ Unity Project/Prototype1/Assets/Scripts/Player Scripts/PlayerHealth.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour {
+
+    public int currentHealth;
+
+    [SerializeField] private int maxHealth = 100;
+    [SerializeField] private int ProjectileDamage = 10;
+
+    private void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    void OnCollisionEnter2D(Collision2D collider)
+    {
+        currentHealth -= ProjectileDamage;
+        Destroy(collider.gameObject);
+        // This is where the attacking player's turn would end, 
+        // assuming that we only shoot one projectile per turn.
+        // Power-ups could increase the number of projectiles,
+        // so maybe change the location of this green text wall.
+    }
+
+}
+
