ac2f7a9be7246443d77197cf903d2a0b40f34ab2
yhjkl <zxdcv>
2018-02-21 12:27:47 +0000

    Physics alterations
    Added a script that breaks the blocks when the projectile hits them, with a 1/4 chance. This can be changed later if needed.

---
+++ Unity Project/Prototype1/Assets/Scripts/Player Scripts/BlockBreaking.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockBreaking : MonoBehaviour {
+
+    [SerializeField] private string tagName;
+    [SerializeField] private float BreakChance;
+
+    void OnCollisionEnter2D(Collision2D collider) {
+        if (collider.gameObject.tag == tagName)
+        {
+            BreakChance = Random.Range(0, 1);
+            if (BreakChance <= 0.25)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
