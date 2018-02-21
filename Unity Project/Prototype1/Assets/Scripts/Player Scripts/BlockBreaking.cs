ecd990e81aba5abf80c3fde45390cafa522521f7
yhjkl <zxdcv>
2018-02-21 14:58:24 +0000

    Fixed the random destruction chance.
    As above - Daniel Smith

--- Unity Project/Prototype1/Assets/Scripts/Player Scripts/BlockBreaking.cs
+++ Unity Project/Prototype1/Assets/Scripts/Player Scripts/BlockBreaking.cs
@@ -4,16 +4,24 @@ using UnityEngine;
 
 public class BlockBreaking : MonoBehaviour {
 
-    [SerializeField] private string tagName;
+    [SerializeField] private string TagName;  
+    //Editable 'tag' attached to the block.
     [SerializeField] private float BreakChance;
+    //Used to determine if the block is broken or not in a collision.
 
-    void OnCollisionEnter2D(Collision2D collider) {
-        if (collider.gameObject.tag == tagName)
+    void OnCollisionEnter2D(Collision2D collider) {     
+        //When the projectile hits a block,
+        if (collider.gameObject.tag == TagName)
+        //If the projectile has the same tag as the block's 'tag',
         {
-            BreakChance = Random.Range(0, 1);
+            BreakChance = Random.Range(0f, 1f);
+            Debug.Log(BreakChance.ToString());
+            //A random number between 0 and 1 is generated.
             if (BreakChance <= 0.25)
+            //If this number is less than or equal to 0.25,
             {
                 Destroy(this.gameObject);
+                //Destroy the block (not the projectile).
             }
         }
     }
--- Unity Project/Prototype1/Assets/Scripts/Player Scripts/PlayerHealth.cs
+++ Unity Project/Prototype1/Assets/Scripts/Player Scripts/PlayerHealth.cs
@@ -7,20 +7,29 @@ public class PlayerHealth : MonoBehaviour {
     public int currentHealth;
 
     [SerializeField] private int maxHealth = 100;
+    //Sets the maximum health of the player's core.
     [SerializeField] private int ProjectileDamage = 10;
+    //Sets the amount of damage that a projectile does to the core.
     [SerializeField] private string tagName;
+    //Editable tag used to determine when damage is dealt.
 
     private void Start()
     {
         currentHealth = maxHealth;
+        //Resets the current health to the maximum.
     }
 
     void OnCollisionEnter2D(Collision2D collider)
+        //When a projectile hits the core,
     {
         if (collider.gameObject.tag == tagName)
+            //If the projectile's tag is the same as the core's 'tag',
         {
             currentHealth -= ProjectileDamage;
+            //Decrease the current health by the projectile damage.
             Destroy(collider.gameObject);
+            //Destroy the projectile.
+
             // This is where the attacking player's turn would end, 
             // assuming that we only shoot one projectile per turn.
             // Power-ups could increase the number of projectiles,
