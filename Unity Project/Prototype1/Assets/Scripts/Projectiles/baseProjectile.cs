d952fbeaa9a517a88df568b09435611a39206598
Daniel Pokladek <danielpokladek@gmail.com>
2018-02-26 15:44:40 +0000

    Created baseProjectile and testProjectile
    Created a baseProjectile script that we will use to inherit from, it will contain all the variables for projectiles and the functionality. Anything that is happening in baseProjectile will happen in testProjectile.
    
    testProjectile inherits from baseProjectile. Both scripts will expand when needed.

---
+++ Unity Project/Prototype1/Assets/Scripts/Projectiles/baseProjectile.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Base Projectile Script
+ * 
+ * This is the base projectile script, we use this as a framework for our projectiles; this will have all the basic variables.
+ * We then create our projectiles that will inherit from this script, and we assign variables on them.
+ * 
+ * Anything we create here will happen in the object that inherits from this script. For example, below I declare in Update that
+ * when we are below '-10' on 'Y' axis we destroy the game object. This will happen on any object that inherits from this script.
+ * 
+ * Unless we override the function the behaviour will stay the same as it is.
+ * 
+ * This way we can have multiple projectiles, and we don't have to decleare the variables for each of them; we only create them once,
+ * and after that we assign the values and we add any functionality if required.
+ * 
+ */
+
+public class baseProjectile : MonoBehaviour {
+
+	[Header("Projectile Variables")]
+    [Tooltip("This is how much damage the projectile will deal.")]
+    [SerializeField] private float projectileDamage;
+
+
+    void Update()
+    {
+        if (this.transform.position.y <= -10)
+            Destroy(this.gameObject);
+    }
+}
