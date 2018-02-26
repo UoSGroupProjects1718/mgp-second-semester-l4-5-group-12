69e93724c57196994b00d7b3021e9b136337bdc6
Daniel Smith <s189693@uos.ac.uk>
2018-02-26 13:02:51 +0000

    More physics and starting UI
    Tested out a couple of shapes and collider types. Adding a new shape is simple.
    Also working on a menu screen, but having some problems making it functional.

---
+++ Unity Project/Prototype1/Assets/Scripts/UI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIController : MonoBehaviour {
+
+    public void OnPlayButtonClick()
+    {
+        SceneManager.LoadScene((int)Scenes.menuScreenTest);
+    }
+
+    public enum Scenes
+    {
+        menuScreenTest,
+        test01
+    }
+
+    //public void OnQuitClick()         // Ignore this bit for now.
+    //{
+    //    Application.Quit();
+    //}
+}
