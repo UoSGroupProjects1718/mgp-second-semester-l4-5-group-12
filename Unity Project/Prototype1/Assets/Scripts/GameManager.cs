c804541e97222b5dbf82cfceff82465b1311d165
Daniel Pokladek <danielpokladek@gmail.com>
2018-05-01 12:48:51 +0100

    Updated the project, and fixed few bugs.
    Due to the new background, we're using sorting layers. This means some other objects were invisible, they are all visible now.

--- Unity Project/Prototype1/Assets/Scripts/GameManager.cs
+++ Unity Project/Prototype1/Assets/Scripts/GameManager.cs
@@ -101,6 +101,7 @@ public class GameManager : MonoBehaviour
         currentIntervalTime = intervalTime;
         Physics2D.IgnoreLayerCollision(0, 9, true);
         Physics2D.IgnoreLayerCollision(8, 9, true);
+
         ChangeTurn();
 	}
 
@@ -165,16 +166,21 @@ public class GameManager : MonoBehaviour
 
     private void Update () 
     {
-        Debug.Log(isGameOver);
-
+        // The camera is not moving.
         if (!cameraMoving && turnText.isActiveAndEnabled && !isProjectile) 
         {
             timerText.enabled = false; 
-            turnText.enabled = false; 
+            turnText.enabled = false;
+
             canShoot = true;
+        }
+
+        if (!cameraMoving && currentRoundState != RoundState.INTERMISSION)
+        {
             isCountingDown = true;
         }
 
+        // Count down if its intermission.
         if (intermissionCounter == true)
         {
             currentIntervalTime -= Time.deltaTime;
@@ -189,6 +195,7 @@ public class GameManager : MonoBehaviour
             }
         }
 
+        // Round time counter.
         if (isCountingDown == true)
         {
             currentTimeLimit -= Time.deltaTime;
@@ -217,13 +224,14 @@ public class GameManager : MonoBehaviour
 
         // Change turn currentTimeLimit.
         currentIntervalTime = intervalTime;
-        canShoot = false;
+        currentTimeLimit = timeLimit;
+
         intermissionCounter = true;
         turnText.enabled = true;
         timerText.enabled = true;
-        currentTimeLimit = timeLimit;
+
+        canShoot = false;
         isCountingDown = false;
-        currentTimeLimit = timeLimit;
     }
 
     //TURN CHANGING
@@ -237,7 +245,6 @@ public class GameManager : MonoBehaviour
             CameraManager.CMInstance.MoveCamera(playerOneCameraPos, playerOneCamSize);
 
             playerTurn += 1;
-            //canShoot = true;
             turnText.text = "Player 1 Turn.";
         }
         else if (playerTurn == 1)
@@ -246,7 +253,6 @@ public class GameManager : MonoBehaviour
             CameraManager.CMInstance.MoveCamera(playerTwoCameraPos, playerTwoCamSize);
 
             playerTurn += 1;
-            //canShoot = true;
             turnText.text = "Player 2 Turn.";
         }
         else if (playerTurn == 2)
@@ -255,7 +261,6 @@ public class GameManager : MonoBehaviour
             CameraManager.CMInstance.MoveCamera(playerOneCameraPos, playerOneCamSize);
 
             playerTurn += 1;
-            //canShoot = true;
             turnText.text = "Player 1 Turn.";
         }
 
@@ -264,8 +269,8 @@ public class GameManager : MonoBehaviour
         {
             playerTurn = 1;
         }
+
         turnCounter += 1;
-        isCountingDown = false;
         currentTimeLimit = timeLimit;
     }
 
@@ -274,6 +279,7 @@ public class GameManager : MonoBehaviour
         if (isGameOver == true)
         {
             Debug.Log("GameOver");
+            isCountingDown = false;
             gameOverText.SetActive(true);
             restartButton.SetActive(true);
         }
